9a21b77c0cd468ea72d17e7b1850a27aeb5a9de3
Matthias Blochberger <matthias.blochberger@posteo.at>
2022-10-17 20:21:21 +0200

    static plots

--- simulations/static.py
+++ simulations/static.py
@@ -209,16 +209,16 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     data = result.df.groupby(["alg", "gamma", "iters"])
 
     # %%
+    conv_frames = 500
     styles = ["-v", "-+", "-x", "-s", "-o", "k-"]
     fig = plt.figure(figsize=utils.set_size(245, 1.0, (1, 1), 0.5))
     # plt.title(f"SNR={cfg.variables['SNR'][0]}dB")
-    plt.xlabel(r"Mixing factor $\gamma$ [1]")
+    plt.xlabel(r"Mixing factor $\gamma_i$ [1]")
     plt.ylabel("Avg. NPM [dB]")
-
     plt.plot(
         cfg.variable_values[2]["gamma"],
         20
-        * np.log10(data.median().T.tail(200).mean()["opt", 1.0, 1])
+        * np.log10(data.median().T.tail(conv_frames).mean()["opt", 1.0, 1])
         * np.ones_like(cfg.variable_values[2]["gamma"]),
         styles.pop(),
         label=f"optimal",
@@ -229,7 +229,7 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     plt.plot(
         cfg.variable_values[2]["gamma"],
         20
-        * np.log10(data.median().T.tail(200).mean()["davgad", 1.0, 1])
+        * np.log10(data.median().T.tail(conv_frames).mean()["davgad", 1.0, 1])
         * np.ones_like(cfg.variable_values[2]["gamma"]),
         styles.pop(),
         label=rf"adaptive $\gamma_i,\,K=1$",
@@ -239,7 +239,7 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     )
     plt.plot(
         cfg.variable_values[2]["gamma"],
-        20 * np.log10(data.median().T.tail(200).mean()["davg", :, 1]),
+        20 * np.log10(data.median().T.tail(conv_frames).mean()["davg", :, 1]),
         styles.pop(),
         label=rf"fixed $\gamma_i,\,K=1$",
         markersize=4,
@@ -248,7 +248,7 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     )
     plt.plot(
         cfg.variable_values[2]["gamma"],
-        20 * np.log10(data.median().T.tail(200).mean()["davg", :, 2]),
+        20 * np.log10(data.median().T.tail(conv_frames).mean()["davg", :, 2]),
         styles.pop(),
         label=rf"fixed $\gamma_i,\,K=2$",
         markersize=4,
@@ -257,7 +257,7 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     )
     plt.plot(
         cfg.variable_values[2]["gamma"],
-        20 * np.log10(data.median().T.tail(200).mean()["davg", :, 10]),
+        20 * np.log10(data.median().T.tail(conv_frames).mean()["davg", :, 10]),
         styles.pop(),
         label=rf"fixed $\gamma_i,\,K=10$",
         markersize=4,
@@ -272,6 +272,9 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     plt.tight_layout(pad=0.5)
     plt.show()
 
+    ax = plt.gca()
+    box = ax.get_position()
+
     # %%
     utils.savefig(fig, "icassp2023-static", format="pgf", pgf_font="serif")
 
@@ -279,11 +282,21 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     # Plot
     mavg = 200
     styles = ["-+", "-x", "-<", "->", "-v", "-s", "-o", "k-"]
-    fig = plt.figure(figsize=utils.set_size(245, 1.0, (1, 1), 0.5))
+    fig = plt.figure(figsize=utils.set_size(400, 1.0, (1, 1), 0.5))
     # plt.title("Title")
     plt.xlabel("Time [frames]")
     plt.ylabel("NPM [dB]")
     # #########
+    plt.fill_between(
+        [6000 - conv_frames, 6000],
+        [-40, -40],
+        [0, 0],
+        facecolor="tab:gray",
+        hatch="//",
+        edgecolor="k",
+        linewidth=0.5,
+        alpha=0.5,
+    )
     (line,) = plt.plot(
         20 * np.log10(data.median().T["opt", 1.0, 1]),
         "k-",
@@ -427,6 +440,8 @@ if __name__ == "__main__":  # Necessary for module loading in condor processes :
     plt.ylim(-40, 0)
     plt.xlim(0, 6000)
     plt.tight_layout(pad=0.5)
+    ax = plt.gca()
+    ax.set_position(box)
     plt.show()
     # %%
     utils.savefig(fig, "icassp2023-static-time", format="pgf", pgf_font="serif")
